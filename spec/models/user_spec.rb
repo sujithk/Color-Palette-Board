b94f71aa37ca42d18b6a1e6aa42fcc8857054ceb
Sujith K <sujith.k@mobme.in>
2018-12-05 13:24:22 +0530

    Rspec for User created

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:username) }
+    it { is_expected.to validate_presence_of(:ip_address) }
+   end
+
+end
