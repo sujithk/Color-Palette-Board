b71984bc48076e9df02225b5ec24da66ccb07a0e
Sujith K <sujith.k@mobme.in>
2018-12-07 10:06:49 +0530

    home controller rspec udpated

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,19 +3,22 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
 
-  describe "GET User DashBoard" do
-    it "has 200 status and renders userboard view" do
-      get :userboard
+  describe "GET Index" do
+    it "renders the index template and returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
       expect(response.status).to eq(200)
-      expect(response).to render_template("userboard")
+      expect(response).to render_template("index")
     end
   end
 
-  describe "GET Index" do
-    it "renders the index template" do
-      get :index
+  describe "GET User DashBoard" do
+    it "has 200 status and renders userboard view" do
+      get :userboard
+      expect(assigns(:users)).not_to be_nil
+      expect(response).to have_http_status(:success)
       expect(response.status).to eq(200)
-      expect(response).to render_template("index")
+      expect(response).to render_template("userboard")
     end
   end
 
@@ -29,7 +32,15 @@ RSpec.describe HomeController, type: :controller do
       expect(response).to render_template(:update_board_tile_color)
       expect(response.headers["Content-Type"]).to eq "text/javascript; charset=utf-8"
     end
+  end
 
+  describe "GET reload_tiles" do
+    it "initializes grid cells variable and renders reload tiles" do
+
+      get :reload_tiles, xhr: true
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:reload_tiles)
+    end
   end
 
 end
