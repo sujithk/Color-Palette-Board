2c9ed2f4f7cb434f49873868bcbf35f29c4ac437
Sujith K <sujith.k@mobme.in>
2018-12-16 15:35:37 +0530

    model spec updated

--- spec/models/palette_spec.rb
+++ spec/models/palette_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Palette, type: :model do
+
+  describe "Creation" do
+    it 'is invalid when color is not in hex format' do
+      invalid_hex_colors = ['123', '#1234', '#12345g', '@123456', '', ' ']
+
+      invalid_hex_colors.each do |invalid_hex_color|
+        grid_cell = Palette.new(code: invalid_hex_color)
+        expect(grid_cell).to be_invalid
+      end
+    end
+  end
+
   context 'validations' do
     it { is_expected.to validate_presence_of(:code) }
     it { is_expected.to validate_presence_of(:row) }
     it { is_expected.to validate_presence_of(:column) }
      it { should belong_to(:user) }
    end
+
 end
